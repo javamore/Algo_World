fa2137f1e12d83091f35ec551912c21896bc48aa
WayJ Mo <javamore@live.com>
2020-04-14 04:15:59 -0700

    Update Unique Paths Series.py

--- DP/Unique Paths Series.py
+++ DP/Unique Paths Series.py
@@ -49,7 +49,6 @@ if __name__ == "__main__":
 #time: O(m*n)  space: O(n)
 
 
-
 """
 Follow up for "Unique Paths":
 Now consider if some obstacles are added to the grids. How many unique paths would there be?
@@ -67,7 +66,8 @@ class Solution2:
     @param obstacleGrid: A list of lists of integers
     @return: An integer
     """
-    def uniquePathsWithObstacles(self, obstacleGrid):
+    #using rolling array
+    def uniquePathsWithObstacles1(self, obstacleGrid):
         # write your code here
         if not obstacleGrid or len(obstacleGrid) == 0 or not obstacleGrid[0] or len(obstacleGrid[0]) == 0:
             return 0
@@ -90,5 +90,29 @@ class Solution2:
                     dp[j] = dp[j - 1]
         
         return dp[n - 1]
+#time: O(m*n), space: O(n)
 
+    #using 2D array
+    def uniquePathsWithObstacles2(self, obstacleGrid):
+        if not obstacleGrid or len(obstacleGrid) == 0 or not obstacleGrid[0] or len(obstacleGrid[0]) == 0:
+            return 0
+        
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0 for j in range(n)] for i in range(m)]
+        dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                    continue
+                
+                if i > 0:
+                    dp[i][j] += dp[i - 1][j]
+                
+                if j > 0:
+                    dp[i][j] += dp[i][j - 1]
+        
+        return dp[m - 1][n - 1]
+#space: O(n^2), time: O(m*n)
 
